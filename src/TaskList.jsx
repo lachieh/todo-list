45bc2131f078ad07e385fb9a901c4812072547d1
Lachlan Heywood <lachlan@realtheory.io>
2020-06-25 14:15:41 -0400

    update with visibility reducer

--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -1,49 +1,55 @@
 import React, { Component } from 'react';
-import Task from './Task';
-import { toggleTodo, deleteTodo } from './redux/actions/todoActions';
-
 import { connect } from 'react-redux';
+
+import Task from './Task';
+
+import { toggleTodo, deleteTodo } from './redux/actions/todoActions';
+import { setVisibility } from './redux/actions/visibilityActions';
 
 class TaskList extends Component {
   render() {
     return (
       <div>
-        <h2 className="TaskList__title">Incomplete Tasks</h2>
-        {this.props.tasks.map((task, i) => {
-          if (!task.complete) {
-            return (
-              <Task
-                task={task}
-                key={i}
-                toggleCompleteStatus={() => {
-                  this.props.toggleCompleteStatus(i);
-                }}
-                deleteTask={() => {
-                  this.props.deleteTask(i);
-                }}
-              />
-            );
-          }
-          return '';
-        })}
-        <h2 className="TaskList__title">Complete Tasks</h2>
-        {this.props.tasks.map((task, i) => {
-          if (task.complete) {
-            return (
-              <Task
-                task={task}
-                key={i}
-                toggleCompleteStatus={() => {
-                  this.props.toggleCompleteStatus(i);
-                }}
-                deleteTask={() => {
-                  this.props.deleteTask(i);
-                }}
-              />
-            );
-          }
-          return '';
-        })}
+        <button onClick={() => { this.props.setVisibility('incomplete')}}>Incomplete</button>
+        <button onClick={() => { this.props.setVisibility('complete')}}>Complete</button>
+        { this.props.visibility === 'incomplete' && (
+          <>
+            <h2 className="TaskList__title">Incomplete Tasks</h2>
+            {this.props.tasks.map((task, i) => {
+              if (!task.complete) {
+                return (
+                  <Task
+                    task={task}
+                    key={i}
+                    toggleCompleteStatus={() => {
+                      this.props.toggleCompleteStatus(i);
+                    }}
+                    deleteTask={() => {
+                      this.props.deleteTask(i);
+                    }}
+                  />
+                );
+              }
+              return '';
+            })}
+          </>
+        ) }
+        { this.props.visibility === 'complete' && (
+          <>
+            <h2 className="TaskList__title">Complete Tasks</h2>
+            {this.props.tasks.map((task, i) => {
+              if (task.complete) {
+                return (
+                  <Task task={task} key={i}
+                    toggleCompleteStatus={() => {this.props.toggleCompleteStatus(i); }}
+                    deleteTask={() => { this.props.deleteTask(i); }}
+                  />
+                );
+              }
+              return '';
+            })}
+          </>
+        )}
       </div>
     );
   }
@@ -51,13 +57,15 @@ class TaskList extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    tasks: state.tasks
+    tasks: state.todoState.tasks,
+    visibility: state.visibility
   }
 }
 
 const mapDispatchToProps = {
   toggleCompleteStatus: toggleTodo,
   deleteTask: deleteTodo,
+  setVisibility: setVisibility,
 }
 
 export default connect(
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,63 +1,10 @@
-const initialState = {
-  tasks: [
-    {
-      name: 'Test Redux Task',
-      complete: false,
-    }
-  ],
-}
+import { combineReducers } from 'redux';
+import todoReducer from './todoReducer';
+import visibilityReducer from './visibilityReducer';
 
-function todoReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
-      return {
-        ...state,
-        tasks: [
-          ...state.tasks,
-          {
-            name: action.content,
-            completed: false,
-          }
-        ]
-      }
-    
-    case 'TOGGLE_TODO':
-      console.log(action);
-      // find task to update
-      const newTask = {...state.tasks[action.index]};
-      // flip completed status of task
-      newTask.complete = !newTask.complete;
+const rootReducer = combineReducers({
+  todoState: todoReducer,
+  visibility: visibilityReducer
+});
 
-      // insert new task into array at position of original
-      const newTasks = state.tasks.map((task, index) => {
-        if (index === action.index) {
-          return newTask
-        }
-        return task;
-      });
-      console.log(newTasks);
-
-      // return a new state object with the updated details
-      return {
-        ...state,
-        tasks: newTasks,
-      }
-
-    case 'DELETE_TODO':
-      const remainingTasks = state.tasks.filter((task, index) => {
-        if (index !== action.index) {
-          return true
-        }
-        return false;
-      });
-      return {
-        ...state,
-        tasks: remainingTasks
-      }
-  
-    default:
-      return state;
-  }
-}
-
-export default todoReducer;
+export default rootReducer;
